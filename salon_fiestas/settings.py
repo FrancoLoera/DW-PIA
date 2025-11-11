209773a2f059264abd41907aa59006920817bc40
FrancoLoera <theprivate_cloud@hotmail.com>
2025-11-11 03:51:13 -0600

    Avances en secciones. Diseño de secciones, modo oscuro y aumento de tamaño de tipografía pendientes

--- salon_fiestas/settings.py
+++ salon_fiestas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7!cu2k+q5vlnd&%_7&tb@#t#xbq^69$q9=9r4y%b#=zh58s60x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.47', 'localhost', '127.0.0.1']
 
 
 # Application definition
